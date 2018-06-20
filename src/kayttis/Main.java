a4f9a3c3ee8a3644c9860547a82b06a2a743d2a4
pirjotuulia <pirjoleppanen@nic.fi>
2018-06-20 15:27:00 +0300

    uusi kayttis/main

--- src/kayttis/Main.java
+++ src/kayttis/Main.java
@@ -12,13 +12,18 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
-        System.out.println("Mitä peliä haluat pelata? Hedelmäpeli, Suomi-visa vai Tietopeli? (H/S/T)");
-        Kysymystenlukija kys = new Kysymystenlukija();
-        List<Kysymys> kysymykset = kys.kysymyslista();
-        Kayttoliittyma kl = new Kayttoliittyma(kysymykset);
-        kl.kaynnista();
-
-//        Tekstikayttoliittyma t = new Tekstikayttoliittyma();
-//        t.kaynnista();
+        System.out.println("Mitä peliä haluat pelata? Hedelmäpeli, Suomi-visa vai Tietopeli? (h/s/t)");
+        String vastaus = lukija.nextLine();
+        if (vastaus.equals("h")) {
+            //käynnistä Saanan peli;
+        } else if (vastaus.equals("s")) {
+            Kysymystenlukija kys = new Kysymystenlukija();
+            List<Kysymys> kysymykset = kys.kysymyslista();
+            Kayttoliittyma kl = new Kayttoliittyma(kysymykset);
+            kl.kaynnista();
+        } else if (vastaus.equals("t")) {
+            Tekstikayttoliittyma t = new Tekstikayttoliittyma();
+            t.kaynnista();
+        }
     }
 }
