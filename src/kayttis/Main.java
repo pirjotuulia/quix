e641583c2730ff329227cfb88aadb4fae032382d
pirjotuulia <pirjoleppanen@nic.fi>
2018-06-20 15:01:54 +0300

    uudempi main

--- src/kayttis/Main.java
+++ src/kayttis/Main.java
@@ -6,10 +6,13 @@ import kysymykset.Kysymystenlukija;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+        System.out.println("Mitä peliä haluat pelata? Hedelmäpeli, Suomi-visa vai Tietopeli? (H/S/T)");
         Kysymystenlukija kys = new Kysymystenlukija();
         List<Kysymys> kysymykset = kys.kysymyslista();
         Kayttoliittyma kl = new Kayttoliittyma(kysymykset);
